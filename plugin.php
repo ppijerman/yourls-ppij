b2c92aadfa877acdbcc2c6272dc90a77d6bf7309
Yudhistira Wibowo <yudhistira.wibowo@itsmeyaw.id>
2022-07-29 15:19:56 +0200

    Initialize Plugin project

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: YOURLS PPI Jerman Plugin
+Plugin URI: https://github.com/ppijerman/
+Description: Custom plugin for PPI Jerman
+Version: 0.1
+Author: PPI Jerman
+Author URI: https://ppijerman.org/
+*/
+
+
